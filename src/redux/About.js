bd01ffd74fd3f6c886ec778773aedb5d72894363
Srinath <reddyysrinath@gmail.com>
2022-06-15 18:00:45 +0530

    txt only changed

--- src/redux/About.js
+++ src/redux/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <p>Flipkart is an Indian e-commerce company, headquartered in Bangalore, Karnataka, India, and incorporated in Singapore as a private limited company. The company initially focused on online book sales before expanding into other product categories such as consumer electronics, fashion, home essentials, groceries, and lifestyle products.</p>
-      <p>All of us assffs love to online shopping as in world who does not want to get his/her work done while sitting comfortably at home? Shopping and buying necessities have been made less time consuming, less expensive and much easier and comfortable for us by online e-commerce platforms. It could not have been possible if these e-commerce platforms were not available. One of these major platforms is Flipkart which is grabbing a larger share of the e-commerce market. So let us now read some facts about the leading online selling store Flipkart and its co-founders Sachin Bansal and Binny Bansal.</p>
+      <p>All of us assffs love to shopping as in world who does not want to get his/her work done while sitting comfortably at home? Shopping and buying necessities have been made less time consuming, less expensive and much easier and comfortable for us by online e-commerce platforms. It could not have been possible if these e-commerce platforms were not available. One of these major platforms is Flipkart which is grabbing a larger share of the e-commerce market. So let us now read some facts about the leading online selling store Flipkart and its co-founders Sachin Bansal and Binny Bansal.</p>
     </div>
   )
 }
